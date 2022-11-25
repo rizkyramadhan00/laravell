6259d49fbe9083d738d1963afc6f87de6eba6ad2
rizkyramadhan00 <zalzalfaridzal@gmail.com>
2022-11-25 09:54:32 +0700

    update laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        h1{
+            color: rgb(252, 0, 0);
+        }
+    </style>
+
+</head>
+<body>
+<h1> laravel </h1>    
+</body>
+</html>
